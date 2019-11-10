d6518504fa0f56f7ca6cecf671025bc267a69a21
Nimrod Dayan <nimrod.dayan@gmail.com>
2019-11-10 23:12:27 +0200

    Changed build metrics plugin check to log error instead of fail build

--- buildmetrics-publisher-api/src/main/java/com/nimroddayan/buildmetrics/plugin/Plugin.kt
+++ buildmetrics-publisher-api/src/main/java/com/nimroddayan/buildmetrics/plugin/Plugin.kt
@@ -1,19 +1,14 @@
 package com.nimroddayan.buildmetrics.plugin
 
-import org.gradle.api.Action
-import org.gradle.api.GradleException
+import mu.KotlinLogging
 import org.gradle.api.Project
 
-const val BUILD_METRICS_PLUGIN_ID = "com.nimroddayan.gradle.build.metrics"
-const val BUILD_METRICS_EXTENSION_NAME = "buildMetrics"
+private val log = KotlinLogging.logger {}
+
+const val BUILD_METRICS_RUNTIME_PLUGIN_ID = "com.nimroddayan.buildmetrics.runtime"
 
 fun Project.checkBuildMetricsPluginApplied() {
-    if (!pluginManager.hasPlugin(BUILD_METRICS_PLUGIN_ID)) {
-        throw GradleException("Build Metrics plugin is not applied")
+    if (!pluginManager.hasPlugin(BUILD_METRICS_RUNTIME_PLUGIN_ID)) {
+        log.warn { "Build Metrics plugin is not applied" }
     }
-}
-
-@Suppress("UnstableApiUsage")
-fun <T>Project.addBuildMetricsPublisherExtension(action: Action<out T>) {
-    extensions.configure(BUILD_METRICS_EXTENSION_NAME, action)
 }
