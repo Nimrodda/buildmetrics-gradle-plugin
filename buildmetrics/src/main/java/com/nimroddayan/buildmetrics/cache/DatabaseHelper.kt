514cec1b80f7d088407066756ed8688f47c2baef
Nimrod Dayan <nimrod.dayan@gmail.com>
2019-11-10 22:30:30 +0200

    Modified db helper to create database only if it doesn't exist

--- buildmetrics/src/main/java/com/nimroddayan/buildmetrics/cache/DatabaseHelper.kt
+++ buildmetrics/src/main/java/com/nimroddayan/buildmetrics/cache/DatabaseHelper.kt
@@ -1,14 +1,25 @@
 package com.nimroddayan.buildmetrics.cache
 
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
+import mu.KotlinLogging
+
+private val log = KotlinLogging.logger {}
 
 class DatabaseHelper(
     dbFile: String? = ".buildmetrics"
 ) {
-    private val driver = JdbcSqliteDriver("jdbc:sqlite:${dbFile ?: ""}")
-    val database = BuildMetricsDb(driver)
+    val database: BuildMetricsDb
 
     init {
-        BuildMetricsDb.Schema.create(driver)
+        Class.forName("org.sqlite.JDBC") // fix for "no driver found"
+        val driver = JdbcSqliteDriver("jdbc:sqlite:${dbFile ?: ""}")
+        database = try {
+            log.debug { "Instantiating database instance" }
+            BuildMetricsDb(driver)
+        } catch (e: Exception) {
+            log.debug { "Failed to instantiate database instance, attempting to create it" }
+            BuildMetricsDb.Schema.create(driver)
+            BuildMetricsDb(driver)
+        }
     }
 }
