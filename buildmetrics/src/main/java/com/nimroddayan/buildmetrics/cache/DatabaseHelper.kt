48dd199119f4d63acc3b366679c65e57da01d9a6
Nimrod Dayan <nimrod.dayan@gmail.com>
2019-11-12 00:24:54 +0200

    Fixed DB creation

--- buildmetrics/src/main/java/com/nimroddayan/buildmetrics/cache/DatabaseHelper.kt
+++ buildmetrics/src/main/java/com/nimroddayan/buildmetrics/cache/DatabaseHelper.kt
@@ -14,11 +14,12 @@ class DatabaseHelper(
         Class.forName("org.sqlite.JDBC") // fix for "no driver found"
         val driver = JdbcSqliteDriver("jdbc:sqlite:${dbFile ?: ""}")
         database = try {
-            log.debug { "Instantiating database instance" }
+            log.debug { "Attempting to create database" }
+            BuildMetricsDb.Schema.create(driver)
+            log.debug { "Database created successfully, returning database instance" }
             BuildMetricsDb(driver)
         } catch (e: Exception) {
-            log.debug { "Failed to instantiate database instance, attempting to create it" }
-            BuildMetricsDb.Schema.create(driver)
+            log.debug { "Database already exists, returning instance" }
             BuildMetricsDb(driver)
         }
     }
