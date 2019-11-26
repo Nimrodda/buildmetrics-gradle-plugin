1236172c85c9aaa9e0b914956b14558dd4004952
Nimrod Dayan <nimrod.dayan@gmail.com>
2019-11-26 22:59:46 +0200

    Moved all deps to Deps.kt

--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -16,12 +16,25 @@
  */
 
 object Deps {
-    private const val okhttpVersion = "4.0.0"
-    
-    const val kotlinVersion = "1.3.50"
+    private const val okhttpVersion = "4.2.2"
+    private const val moshiVersion = "1.9.1"
+    private const val retrofitVersion = "2.6.2"
+    const val sqldelightVersion = "1.2.0"
+    const val kotlinVersion = "1.3.60"
     
     const val okhttp = "com.squareup.okhttp3:okhttp:$okhttpVersion"
     const val okhttpLogging = "com.squareup.okhttp3:logging-interceptor:$okhttpVersion"
     const val junit = "junit:junit:4.12"
     const val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
+    const val moshi = "com.squareup.moshi:moshi:$moshiVersion"
+    const val moshiKotlin = "com.squareup.moshi:moshi-kotlin:$moshiVersion"
+    const val retrofit = "com.squareup.retrofit2:retrofit:$retrofitVersion"
+    const val retrofitMoshi = "com.squareup.retrofit2:converter-moshi:$retrofitVersion"
+    const val kotlinLogging = "io.github.microutils:kotlin-logging:1.7.6"
+    const val mockServer = "com.squareup.okhttp3:mockwebserver:$okhttpVersion"
+    const val truth = "com.google.truth:truth:1.0"
+    const val sqldelight = "com.squareup.sqldelight:sqlite-driver:$sqldelightVersion"
+    const val oshi = "com.github.oshi:oshi-core:4.1.1"
+    const val commonsIo = "commons-io:commons-io:2.6"
+    const val kotlinStd = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
 }
